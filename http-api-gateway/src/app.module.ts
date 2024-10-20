e7ca6c4e564dba21264af2a12f83bc9ab251016f
Nicolás Navarro <nicohnavarro@gmail.com>
2024-10-21 00:16:51 +0200

    Add payment in gateway

--- http-api-gateway/src/app.module.ts
+++ http-api-gateway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { NatsClientModule } from './nats-client/nats-client.module';
+import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [NatsClientModule, UsersModule],
+  imports: [NatsClientModule, UsersModule, PaymentsModule],
   controllers: [],
   providers: [],
 })
---
+++ http-api-gateway/src/payments/payments.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { NatsClientModule } from 'src/nats-client/nats-client.module';
+import { PaymenetsController } from './payments.controller';
+
+@Module({
+  imports: [NatsClientModule],
+  controllers: [PaymenetsController],
+  providers: [],
+})
+export class PaymentsModule {}
